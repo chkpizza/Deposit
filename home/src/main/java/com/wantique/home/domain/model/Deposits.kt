9e8f925d43c2f399311c0ccb21269640361c069d
chkpizza <48915641+chkpizza@users.noreply.github.com>
2023-12-18 16:06:31 +0900

    Horizontal Model 이름 변경

--- home/src/main/java/com/wantique/home/domain/model/Deposits.kt
+++ home/src/main/java/com/wantique/home/domain/model/Deposits.kt
@@ -5,9 +5,9 @@ import com.wantique.base.ui.SimpleSubmittableState
 import com.wantique.home.BR
 import com.wantique.home.R
 
-data class HorizontalDeposit<ITEM: SimpleModel> (
-    val simpleSubmittableState: SimpleSubmittableState<ITEM>,
+data class Deposits<ITEM: SimpleModel> (
     val title: String,
+    val simpleSubmittableState: SimpleSubmittableState<ITEM>,
 ) : SimpleModel {
     override fun layoutId(): Int = R.layout.view_holder_deposit_horizontal
 
--- home/src/main/java/com/wantique/home/ui/HomeViewModel.kt
+++ home/src/main/java/com/wantique/home/ui/HomeViewModel.kt
@@ -8,7 +8,7 @@ import com.wantique.base.ui.BaseViewModel
 import com.wantique.base.ui.SimpleModel
 import com.wantique.base.ui.SimpleSubmittableState
 import com.wantique.home.domain.model.Deposit
-import com.wantique.home.domain.model.HorizontalDeposit
+import com.wantique.home.domain.model.Deposits
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
@@ -18,25 +18,25 @@ class HomeViewModel @Inject constructor(
     private val _home = MutableLiveData<SimpleSubmittableState<SimpleModel>>()
     val home: LiveData<SimpleSubmittableState<SimpleModel>> get() = _home
 
-    private lateinit var horizontalDeposit: HorizontalDeposit<SimpleModel>
+    private lateinit var deposits: Deposits<SimpleModel>
 
     fun test() {
         makeHorizontalDeposit()
 
         _home.value = SimpleSubmittableState<SimpleModel>().apply {
             submitList(mutableListOf<SimpleModel>().apply {
-                add(horizontalDeposit)
+                add(deposits)
             })
         }
     }
 
     private fun makeHorizontalDeposit() {
-        horizontalDeposit = HorizontalDeposit(SimpleSubmittableState<SimpleModel>().apply {
+        deposits = Deposits("은행별 최고 금리 예금 상품", SimpleSubmittableState<SimpleModel>().apply {
             submitList(listOf(
                 Deposit("https://news.koreadaily.com/data/photo/2023/09/21/202309211503778535_650be03e9459a.jpeg", "국민수퍼정기예금(개인)", "1" ,""),
                 Deposit("https://news.koreadaily.com/data/photo/2023/09/21/202309211503778535_650be03e9459a.jpeg", "신한 My플러스 정기예금", "2" ,""),
                 Deposit("https://news.koreadaily.com/data/photo/2023/09/21/202309211503778535_650be03e9459a.jpeg", "NH고향사랑기부예금", "3" ,""),
             ))
-        }, "은행별 최고 금리 예금 상품")
+        })
     }
 }
